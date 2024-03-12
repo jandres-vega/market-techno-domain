e9330e9a760bec033771b1d114aff04b0e9a2278
jandres-vega <jandresvega@itsa.edu.co>
2024-03-11 20:26:57 -0500

    Add project Spring with initializr

---
+++ src/main/java/com/store/marketTechno/MarketTechnoApplication.java
@@ -0,0 +1,13 @@
+package com.store.marketTechno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketTechnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketTechnoApplication.class, args);
+	}
+
+}
