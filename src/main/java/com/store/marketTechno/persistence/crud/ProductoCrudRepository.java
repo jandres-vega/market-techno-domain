4e6bf56b5081e29985aa49ffd6c375c0b7c461dd
jandres-vega <jandresvega@itsa.edu.co>
2024-03-12 19:20:47 -0500

    Add CrudREpository e implement methods to products

---
+++ src/main/java/com/store/marketTechno/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,13 @@
+package com.store.marketTechno.persistence.crud;
+
+import com.store.marketTechno.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
